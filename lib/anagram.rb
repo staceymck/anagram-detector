8d51dc747b8c2a0c168ed92c85497e19d4e40b93
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-15 09:16:01 -0600

    commit tree 0ff9546317fc46591a1a5b4c030ba9df20a77058

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(list)
+        list.select do |word|
+            @word != word && word.downcase.chars.sort == @word.downcase.chars.sort 
+        end
+    end
+end
+
